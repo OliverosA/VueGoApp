a1d2ea7567390eaa9fc17587ba6c9d54ac7b745f
OliverosA <aldair_oliveros09@hotmail.es>
2023-02-06 17:19:21 -0600

    adding Api file

--- titw/titw-fe/src/composables/useEmails.ts
+++ titw/titw-fe/src/composables/useEmails.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
 import { ref } from "vue";
+import emailApi from "@/api/emailApi";
 
 const useEmails = () => {
   const emails = ref([]);
 
   const getEmails = async () => {
-    const { data } = await axios.get(`http://localhost:3000/emails`);
+    const { data } = await emailApi.get(`/emails`);
 
     emails.value = data.hits.hits?.map(
       (email: { _id: string; _source: object }) => {
@@ -18,10 +18,7 @@ const useEmails = () => {
     const body = {
       Term: term,
     };
-    const { data } = await axios.post(
-      `http://localhost:3000/emails/term`,
-      body
-    );
+    const { data } = await emailApi.post(`/emails/term`, body);
     emails.value = data.hits.hits?.map(
       (email: { _id: string; _source: object }) => {
         return { id: email._id, source: email._source };
